6eb72c2a3d6fb14aed310bf0914eaad4bedd795a
Akhil Murali <akhilmural@noreply.com>
2023-05-20 17:39:45 +0530

    Initial Commit

---
+++ src/main/java/com/example/jenkinstut/JenkinsTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTutApplication.class, args);
+    }
+
+}
