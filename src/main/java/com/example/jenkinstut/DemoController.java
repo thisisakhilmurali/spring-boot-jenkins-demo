c3a9e2ae6c48e31bccb7ad7fd75697ced2623438
Akhil Murali <akhilmural@noreply.com>
2023-05-22 09:15:27 +0530

    Second Commit

---
+++ src/main/java/com/example/jenkinstut/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.jenkinstut;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+
+    @GetMapping("/one")
+    public String helloWorld() {
+        return "One Initiated";
+    }
+
+}
